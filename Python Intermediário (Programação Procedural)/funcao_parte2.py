66e3f5e544e9994d44261c570606742232819055
gleyson <gleysonsantos@protonmail.com>
2022-09-04 12:45:32 -0300

    Funcao em python parte 2

--- Python Intermediário (Programação Procedural)/funcao_parte2.py
+++ Python Intermediário (Programação Procedural)/funcao_parte2.py
@@ -3,9 +3,20 @@ Funções - def em Python (Parte 2)
 """
 
 
-def divisao(n1, n2):
-    return [n1, n2]
+# def divisao(n1, n2):
+#     return [n1, n2]
+#
+# divide = divisao(n1=0, n2=0)
+# if divide:
+#     print(divide)
 
-divide = divisao(n1=0, n2=0)
-if divide:
-    print(divide)
+def f(var):
+    print(var)
+
+
+def dumb():
+    return f
+
+
+var = dumb()('Valor passado para função f')
+print(var)
