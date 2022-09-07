76cba5ce0578fc494c0896170548ab19982d7d5d
gleyson <gleysonsantos@protonmail.com>
2022-09-06 22:19:14 -0300

    Default arguments

--- Python Intermediário (Programação Procedural)/funcoes_args_kwargs.py
+++ Python Intermediário (Programação Procedural)/funcoes_args_kwargs.py
@@ -2,11 +2,14 @@
 Funções (def) em Python - *args **args
 """
 
-def func(a1, a2, a3, a4, a5):
+
+def func(a1, a2, a3, a4, a5, nome=None):
     print(a1)
     print(a2)
     print(a3)
     print(a4)
     print(a5)
+    print(nome)
 
-func(1, 2, 3, 4, 5)
+
+func(1, 2, 3, 4, 5, 'Gleyson')
