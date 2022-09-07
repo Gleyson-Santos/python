dcc0a649439641a3d19b0217460269d03ce73965
gleyson <gleysonsantos@protonmail.com>
2022-09-07 15:43:49 -0300

    desenpacotando tuplas

--- Python Intermediário (Programação Procedural)/tuplas.py
+++ Python Intermediário (Programação Procedural)/tuplas.py
@@ -3,8 +3,14 @@ Tuplas em Python
 """
 
 t1 = (1, 2, 3, 'a', 'Gleyson Santos')
+t2 = 6, 7, 8, 9, 10
+t1 += t2
+# for i in t1:
+#     print(i)
 
-print(t1)
-for i in t1:
-    print(i)
-print(type(t1))
+# print(type(t2))
+n1, n2, *_ = t1 * 20
+
+# print(n1)
+# print(n2)
+print(_)
