adb569f93ca420471e8d2ebb18d808c149e2c693
gleyson <gleysonsantos@protonmail.com>
2022-09-06 22:39:53 -0300

    kwargs.get()

--- Python Intermediário (Programação Procedural)/funcoes_args_kwargs.py
+++ Python Intermediário (Programação Procedural)/funcoes_args_kwargs.py
@@ -5,8 +5,10 @@ Funções (def) em Python - *args **args
 
 def func(*args, **kwargs):
     print(args)
-    print(kwargs['nome'], kwargs['sobrenome'])
+    # print(kwargs['nome'], kwargs['sobrenome'])
+    idade = kwargs.get('idade')
+    print(idade)
 
 lista = [1, 2, 3, 4, 5]
 lista2 = [10, 20, 30, 40, 50]
-func(*lista, *lista2, nome='Gleyson', sobrenome='Santos')
+func(*lista, *lista2, nome='Gleyson', sobrenome='Santos', idade=0)
