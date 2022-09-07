191b47ca2625fe62a1f14ab52157620ed2c19588
gleyson <gleysonsantos@protonmail.com>
2022-09-07 11:46:42 -0300

    funções lambda

---
+++ Python Intermediário (Programação Procedural)/funcoes_lambda.py
@@ -0,0 +1,27 @@
+"""
+lambda - Funções anônimas
+"""
+
+
+# def func(arg, arg2):
+#     return arg * arg2
+#
+#
+# var = func(2, 2)
+# print(var)
+
+lista = [
+    ['p1', 13],
+    ['p2', 6],
+    ['p3', 7],
+    ['p4', 50],
+    ['p5', 8],
+]
+
+
+def func(item):
+    return item[1]
+
+
+lista.sort(key=lambda item: item[1], reverse=True)
+print(lista)
