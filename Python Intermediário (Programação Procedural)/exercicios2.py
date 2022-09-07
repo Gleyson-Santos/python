948d78f0739fee94c70f62405c8dba0ec1c6f4d6
gleyson <gleysonsantos@protonmail.com>
2022-09-07 11:36:58 -0300

    exercicios com funções

--- Python Intermediário (Programação Procedural)/exercicios2.py
+++ Python Intermediário (Programação Procedural)/exercicios2.py
@@ -4,17 +4,17 @@ da função2 executada
 """
 
 
-def func1():
-    x = int(input('Digite um número: '))
-    y = int(input('Digite outro número: '))
-    return x + y
-
-
-def func2(arg):
-    return arg()
-
-var = func2(func1)
-print(var)
+# def func1():
+#     x = int(input('Digite um número: '))
+#     y = int(input('Digite outro número: '))
+#     return x + y
+#
+#
+# def func2(arg):
+#     return arg()
+#
+# var = func2(func1)
+# print(var)
 
 """
 2 - Crie uma função1 que recebe uma função2 como parâmetro e retorne o valor da função2
@@ -23,9 +23,19 @@ diferente de argumentos.
 """
 
 
-def fala_oi(nome):
-    return f'Oi {nome}'
+def mestre(func, *args, **kwargs):
+    return func(*args, **kwargs)
+
+
+def fala_oi(nome, *msg2):
+    return f'Oi {nome} {msg2}'
 
 
 def saudacao(nome, msg):
     return f'{msg} {nome}'
+
+
+executando = mestre(fala_oi, 'Gleyson', 'outra msg', 'mais msg', '2', '3')
+executando2 = mestre(saudacao, 'Gleyson', 'Bom dia')
+print(executando)
+print(executando2)
