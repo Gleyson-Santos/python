f5f310bc2e15577c6da3a4519df2ca5837f27059
gleyson <gleysonsantos@protonmail.com>
2022-09-07 15:45:52 -0300

    mudando o valor de um indice de uma tupla

--- Python Intermediário (Programação Procedural)/tuplas.py
+++ Python Intermediário (Programação Procedural)/tuplas.py
@@ -3,14 +3,7 @@ Tuplas em Python
 """
 
 t1 = (1, 2, 3, 'a', 'Gleyson Santos')
-t2 = 6, 7, 8, 9, 10
-t1 += t2
-# for i in t1:
-#     print(i)
-
-# print(type(t2))
-n1, n2, *_ = t1 * 20
-
-# print(n1)
-# print(n2)
-print(_)
+t1 = list(t1)
+t1[0] = 'Arroz'
+t1 = tuple(t1)
+print(t1)
