4c5fddc1623111905e1e22ed0a2753147c0329a7
gleyson <gleysonsantos@protonmail.com>
2022-09-06 22:21:26 -0300

    Acessando valor de retorno pelo indice

--- Python Intermediário (Programação Procedural)/funcoes_args_kwargs.py
+++ Python Intermediário (Programação Procedural)/funcoes_args_kwargs.py
@@ -3,13 +3,16 @@ Funções (def) em Python - *args **args
 """
 
 
-def func(a1, a2, a3, a4, a5, nome=None):
+def func(a1, a2, a3, a4, a5, nome=None, a6=None):
     print(a1)
     print(a2)
     print(a3)
     print(a4)
     print(a5)
+    print(a6)
     print(nome)
+    return nome, a6
 
 
-func(1, 2, 3, 4, 5, 'Gleyson')
+var = func(1, 2, 3, 4, 5, nome='Gleyson')
+print(var[0])
