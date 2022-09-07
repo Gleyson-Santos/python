d7a94a1ab35c7bad67ff941518fdcf3a49c3f339
gleyson <gleysonsantos@protonmail.com>
2022-09-07 10:50:04 -0300

    variaveis globais

--- Python Intermediário (Programação Procedural)/func_parte_4.py
+++ Python Intermediário (Programação Procedural)/func_parte_4.py
@@ -2,14 +2,14 @@ variavel = 'valor'
 
 
 def func():
-    print(variavel)
+    print(variavel, 'func')
 
 
 def func2():
     # variavel = 'outra valor'
     global variavel
     variavel = 'outra valor'
-    print(variavel)
+    print(variavel, 'func2')
 
 
 func()
