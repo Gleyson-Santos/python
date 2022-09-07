7085346adba93a87ff339ab6890c9f5379eaeb79
gleyson <gleysonsantos@protonmail.com>
2022-09-06 22:23:46 -0300

    desempacotando valores

--- Python Intermediário (Programação Procedural)/funcoes_args_kwargs.py
+++ Python Intermediário (Programação Procedural)/funcoes_args_kwargs.py
@@ -3,16 +3,10 @@ Funções (def) em Python - *args **args
 """
 
 
-def func(a1, a2, a3, a4, a5, nome=None, a6=None):
-    print(a1)
-    print(a2)
-    print(a3)
-    print(a4)
-    print(a5)
-    print(a6)
-    print(nome)
-    return nome, a6
+def func(*args):
+    print(args)
 
 
-var = func(1, 2, 3, 4, 5, nome='Gleyson')
-print(var[0])
+lista = [1, 2, 3, 4, 5]
+n1, n2, *n = lista
+print(n1, n2, n)
