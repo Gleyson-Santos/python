b1f500cdf1f1bb37659998da56b1a84825ce1cfa
gleyson <gleysonsantos@protonmail.com>
2022-09-11 17:20:39 -0300

    jogo pergunta e reposta com dicionarios

--- Python Intermediário (Programação Procedural)/sistema_de_perguntas_e_respostas_com_dicionarios.py
+++ Python Intermediário (Programação Procedural)/sistema_de_perguntas_e_respostas_com_dicionarios.py
@@ -6,10 +6,17 @@ perguntas = {
     'Pergunta 2': {
         'pergunta': 'Quanto é 3 + 2? ',
         'reposta': {'a': '1', 'b': '4', 'c': '5', },
-        'reposta_certa': 'C', }, }
-
+        'reposta_certa': 'c', }, }
+resposta_certa = 0
 for pk, pv in perguntas.items():
     print(f'{pk}: {pv["pergunta"]}')
     print('Escolha uma das opções abaixo: ')
     for rk, rv in pv['reposta'].items():
         print(f'[{rk}]: {rv}')
+
+    reposta_usuario = input('Sua resposta: ')
+    if reposta_usuario == pv['reposta_certa']:
+        print('Acertou')
+        resposta_certa += 1
+    else:
+        print('Você errou!!! ')
