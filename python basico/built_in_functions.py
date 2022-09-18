0df81cceee0015fb4da62f9f0c16f50de2abce84
Gleyson Santos <gleysonsantos@protonmail.com>
2022-09-18 19:27:02 -0300

    Review built in functions

--- python basico/built_in_functions.py
+++ python basico/built_in_functions.py
@@ -2,7 +2,7 @@
 Built in functions
 """
 num1 = input('Digite um numero: ')
-num2 = input('Digite outro número')
+num2 = input('Digite outro número ')
 num1_convertido = int(num1) if num1.isdigit() else 'Não pode ser convertido'
 num2_convertido = int(num2) if num1.isdigit() else 'Não pode ser convertido'
 print(num1_convertido)
