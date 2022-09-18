86b3c0fa7cf7c71a9c9734028bcfa913f5305eac
Gleyson Santos <gleysonsantos@protonmail.com>
2022-09-18 19:20:40 -0300

    Review len built in function

--- python basico/len_function.py
+++ python basico/len_function.py
@@ -10,3 +10,9 @@ if qtd_chars < 6:
     print('Você precisa digitar pelo menos, 06 chars')
 else:
     print('Você foi cadastrado no sistema. ')
+
+str1 = input('Digite alguma coisa: ')
+str2 = input('Digite outra coisa: ')
+
+print(f'A quantidade total de caracteres digitados foi {len(str1) + len(str2)}')
+
