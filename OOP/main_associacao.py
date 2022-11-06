b3733914370c7397c3bdd82f8dc3c558b020537d
Gleyson Santos <gleysonsantos@protonmail.com>
2022-11-06 20:08:13 -0300

    add regexp folder

---
+++ OOP/main_associacao.py
@@ -0,0 +1,9 @@
+from associacao import Escritor, Caneta, MaquinaDeEscrever
+
+escritor = Escritor('Juju')
+caneta = Caneta('Bic')
+maquina = MaquinaDeEscrever()
+
+print(escritor.nome)
+print(caneta.marca)
+maquina.escrever()
