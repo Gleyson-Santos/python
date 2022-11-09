b5032d597631e7a6f2390af054faba3232a8aa7a
Gleyson Santos <gleysonsantos@protonmail.com>
2022-11-09 06:36:04 -0300

    nov 09

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-import os
+# def rec(n):
+#     if n == 1 or n == 0:
+#         return 1
+#     return n * rec(n-1)
+#
+#
+# calc = rec(4)
+# print(calc)
 
-
-off = False
-print('Hello, this is a two digit calculator.')
-while not off:
-    os.system('clear')
-    n1 = ''
-    while len(n1) != 2:
-        n1 = input('Type a two digit number, please.\n')
-    total = 0
-    for digit in n1:
-        total += int(digit)
-
-    print(total)
-    if input('Do you want to sum again? ') == 'no':
-        off = True
-        print('Bye bye!!!')
---
+++ regexp/aula8.py
@@ -0,0 +1,10 @@
+import re
+
+texto = '''
+ONLINE  192.168.0.1 GHIJK active
+OFFLINE  192.168.0.2 GHIJK inactive
+OFFLINE  192.168.0.3 GHIJK active
+ONLINE  192.168.0.4 GHIJK active
+ONLINE  192.168.0.5 GHIJK inactive
+OFFLINE  192.168.0.6 GHIJK active
+'''
